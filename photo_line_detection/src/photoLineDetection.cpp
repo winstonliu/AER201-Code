9bb6722075fabc02e6c8684e014c49f02b72f747
winstonliu <winstonl95@hotmail.com>
2015-01-14 16:02:07 -0500

    Proof-of-concept of threshold line detection. Need to separate display code from sensor detection code.

--- photo_line_detection/src/photoLineDetection.cpp
+++ photo_line_detection/src/photoLineDetection.cpp
@@ -4,6 +4,7 @@
 PhotoLineDetection::PhotoLineDetection(int newSensorPin)
 {
 	// Initialize values and set sensor pin mode
+	sensor_thresh = 15;
 	pinMode(newSensorPin, INPUT);
 	sensorPin = newSensorPin;
 	sensor_value = 0;
@@ -13,4 +14,32 @@ int PhotoLineDetection::checkSensor()
 {
 	sensor_value = analogRead(sensorPin);
 	return sensor_value;
+}
+
+void PhotoLineDetection::calibrate(int calibrate_color)
+{
+	switch (calibrate_color)
+	{
+		case WHITE:
+			thresh.white = sensor_value;	
+			break;
+		case RED:
+			thresh.red = sensor_value;
+			break;
+		case BLACK:
+			thresh.black = sensor_value;
+			break;
+	}
+}
+
+int PhotoLineDetection::detect()
+{
+	if (pow(sensor_value - thresh.white, 2) <= pow(sensor_thresh, 2))
+		return WHITE;
+	else if (pow(sensor_value - thresh.red, 2) <= pow(sensor_thresh, 2))
+		return RED;
+	else if (pow(sensor_value - thresh.black, 2) <= pow(sensor_thresh, 2))
+		return BLACK;
+	else 
+		return CALIBRATED;
 }
--- photo_line_detection/src/photo_line_detection.ino
+++ photo_line_detection/src/photo_line_detection.ino
@@ -4,15 +4,81 @@
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 PhotoLineDetection lineDet(A0);
 
+struct button
+{
+	int pin;
+	int state;
+};
+
+// Initialize calibration buttons
+button btnCal;
+button btnToggle;
+
+// Color calibration menu
+// 0 - white
+// 1 - red
+// 2 - black
+// 3 - calibrated
+int cal_color = 0;
+
 void setup()
 {
+	btnCal.pin = 6;
+	btnCal.state = 0;
+
+	btnToggle.pin = 7;
+	btnToggle.state = 0;
+
+	pinMode(btnCal.pin, INPUT);
+	pinMode(btnToggle.pin, INPUT);
 	// Initialize LCD
 	lcd.begin(16,2);
 }
 
 void loop()
 {
+	btnToggle.state = digitalRead(btnToggle.pin);
+	btnCal.state = digitalRead(btnCal.pin);
+
+	// Print sensor values
 	lcd.print(lineDet.checkSensor());
+	lcd.setCursor(0,2);
+
+	if (btnToggle.state == HIGH)
+	{
+		if (cal_color != 3)
+			++cal_color;
+		else
+			cal_color = 0;
+	}
+
+	switch(cal_color)
+	{
+		case WHITE:
+			lcd.print("SET WHITE");
+			break;
+		case RED:
+			lcd.print("SET RED");
+			break;
+		case BLACK:
+			lcd.print("SET BLACK");
+			break;
+		case CALIBRATED:
+			lcd.print(lineDet.detect());
+			break;
+		default:
+			lcd.print("HE'S DEAD JIM"); 
+	}
+
+	if (btnCal.state == HIGH && cal_color != 3)
+	{
+		lineDet.calibrate(cal_color);
+		lcd.print("CALIBRATED");
+		
+		if (cal_color == BLACK)
+			cal_color = CALIBRATED;
+	}
+
 	delay(500);
 	lcd.clear();
 }
