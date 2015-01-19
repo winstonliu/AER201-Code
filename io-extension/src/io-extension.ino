e5d30a1d71951aae3f76393bb779013ff3dd67b8
Chengzhi Winston Liu <liuche25@localhost.localdomain>
2015-01-19 13:19:49 -0500

    Io-extension test with liquid crystal libraries. From now on, will probably just have lcd attached to arduino and io-extensions driving sensor pins.

--- io-extension/src/io-extension.ino
+++ io-extension/src/io-extension.ino
@@ -1,7 +1,14 @@
-#include "Wire.h"
+#include <Wire.h>
+#include <LiquidTWI2.h>
+
+LiquidTWI2 lcd(0x20, 1);
 
 void setup()
 {
+	Serial.begin(9600);
+	lcd.setMCPType(LTI_TYPE_MCP23017);
+	lcd.begin(16,2);
+/*
 	Wire.begin();
 	Wire.beginTransmission(0x20);
 	Wire.write(0x00);
@@ -11,10 +18,17 @@ void setup()
 	Wire.write(0x01);
 	Wire.write(0x00);
 	Wire.endTransmission();
+*/
 }
 
 void loop()
 {
+	lcd.print("Hello, world!");
+	Serial.println("Hello, world");
+	delay(500);
+	lcd.clear();
+	delay(500);
+/*
 	delay(1000);
 	Wire.beginTransmission(0x20);
 	Wire.write(0x13);
@@ -24,4 +38,5 @@ void loop()
 	Wire.write(0x12);
 	Wire.write(0x01);
 	Wire.endTransmission();
+*/
 }
