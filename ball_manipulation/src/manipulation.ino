280284cf94b81e94a28ec27296f144ae6bff8f80
Chengzhi Winston Liu <liuche25@localhost.localdomain>
2015-02-08 18:26:01 -0500

    Actually working.

--- ball_manipulation/src/manipulation.ino
+++ ball_manipulation/src/manipulation.ino
@@ -7,57 +7,57 @@
 #include "rgb_lcd.h"
 
 // --------------------------------------------------------------------------- Motors
-int motor_left[] = {3,2};
+const int MOTOR_LEFT[] = {5,4};
 
-int hopper_sensor = 1;
-int encoder_sensor = 0;
-int ball_collected_sensor = 2;
-int LED = 10;
+const int HOPPER_SENSOR = 3;
+const int ENCODER_SENSOR = 2;
+const int ball_collected_sensor = 7;
+const int LED = 10;
 
 rgb_lcd lcd;
 
-boolean claw_in = true;
-boolean on = false;
-boolean start = true;
+boolean claw_dir = true;
+boolean motor_on = false;
 boolean engaged = false;
+
 
 // --------------------------------------------------------------------------- Setup
 void setup() 
 {
     Serial.begin(9600);
 	lcd.begin(16,2);
-    pinMode(motor_left[0], OUTPUT);
-    pinMode(motor_left[1], OUTPUT);
+    pinMode(MOTOR_LEFT[0], OUTPUT);
+    pinMode(MOTOR_LEFT[1], OUTPUT);
+	pinMode(HOPPER_SENSOR, INPUT);
+	pinMode(ENCODER_SENSOR, INPUT);
 }
 
 void loop()
 {    
+	int buttonstate = digitalRead(HOPPER_SENSOR);
     //Triggers the motor
-    if(read_val(hopper_sensor) > 600 && !on)
+    if(buttonstate == HIGH && !motor_on)
 	{
-		on = true;
+		motor_on = true;
 		Serial.println("Motor starting");
 		lcd.clear();	
 		lcd.print("Motor Start");
-		lcd.setCursor(0,1);
     }
 
-    if(on)
+    if(motor_on)
 	{
-		boolean ret = turn_motor(claw_in);
+		boolean ret = turn_motor(claw_dir);
 		if(ret)
 		{
 			on = false;
-			claw_in = !claw_in;
-			if(!claw_in)
+			claw_dir = !claw_dir;
+			if(!claw_dir)
 			{
 				Serial.println("Motor stopped");
 				lcd.clear();	
 				lcd.print("Motor Stop");
-				lcd.setCursor(0,1);
-				lcd.print(analogRead(hopper_sensor));
 				delay(2000);
-				on = true;
+				motor_on = true;
 				Serial.println("Motor resetting");
 				lcd.clear();	
 				lcd.print("Motor Reset Now");
@@ -80,12 +80,10 @@ void loop()
 		}
     }    
     delay(500);
-	Serial.print("claw_in: ");
-	Serial.print(claw_in);
-	Serial.print(" on: ");
-	Serial.print(on);
-	Serial.print(" start: ");
-	Serial.print(start);
+	Serial.print("claw_dir: ");
+	Serial.print(claw_dir);
+	Serial.print(" motor_on: ");
+	Serial.print(motor_on);
 	Serial.print(" engaged: ");
 	Serial.println(engaged);
 }
@@ -93,6 +91,7 @@ void loop()
 
 //-----Sensors
 //--------------------
+/* OBSOLETE
 int read_val(int pin){
     int sensorValue = analogRead(pin);
     
@@ -104,6 +103,7 @@ int read_val(int pin){
 
     return sensorValue;
 }
+*/
 
 boolean ball_collected(){
 	return true;
@@ -120,8 +120,9 @@ void blink_led(){
 //--------------------
 boolean turn_motor(boolean left)
 {
+	int sensorstate = digitalRead(ENCODER_SENSOR);
 	//Runs the motor
-    if(read_val(encoder_sensor) < 600 && on)
+    if(sensorstate == LOW && motor_on)
 	{
 		if(engaged)
 		{    //Kill motor
@@ -131,7 +132,7 @@ boolean turn_motor(boolean left)
 			return true;
 		}
     }    
-    else if (read_val(encoder_sensor) > 600 && on)
+    else if (sensorstate == HIGH && motor_on)
 	{
 		if(!engaged)
 		{
@@ -154,19 +155,19 @@ boolean turn_motor(boolean left)
 
 void motor_stop()
 {
-    digitalWrite(motor_left[0], LOW);
-    digitalWrite(motor_left[1], LOW);    
+    digitalWrite(MOTOR_LEFT[0], LOW);
+    digitalWrite(MOTOR_LEFT[1], LOW);    
 }
 
 void motor_r()
 {
-    digitalWrite(motor_left[0], HIGH);
-    digitalWrite(motor_left[1], LOW);
+    digitalWrite(MOTOR_LEFT[0], HIGH);
+    digitalWrite(MOTOR_LEFT[1], LOW);
 }
 
 void motor_l()
 {
-    digitalWrite(motor_left[0], HIGH);
-    digitalWrite(motor_left[1], HIGH);    
+    digitalWrite(MOTOR_LEFT[0], HIGH);
+    digitalWrite(MOTOR_LEFT[1], HIGH);    
 }
 
