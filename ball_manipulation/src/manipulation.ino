1fc56c7380a212814333bf0e89bd41ca84955209
Chengzhi Winston Liu <liuche25@localhost.localdomain>
2015-02-08 17:24:13 -0500

    Working ball grabber and debug info.

--- ball_manipulation/src/manipulation.ino
+++ ball_manipulation/src/manipulation.ino
@@ -22,9 +22,10 @@ boolean start = true;
 boolean engaged = false;
 
 // --------------------------------------------------------------------------- Setup
-void setup() {
+void setup() 
+{
     Serial.begin(9600);
-		lcd.begin(16,2);
+	lcd.begin(16,2);
     pinMode(motor_left[0], OUTPUT);
     pinMode(motor_left[1], OUTPUT);
 }
@@ -32,43 +33,61 @@ void setup() {
 void loop()
 {    
     //Triggers the motor
-    if(read_val(hopper_sensor) > 600 && !on){
-      on = true;
-      Serial.println("Motor starting");
-			lcd.clear();	
-			lcd.print("Motor Start");
+    if(read_val(hopper_sensor) > 600 && !on)
+	{
+		on = true;
+		Serial.println("Motor starting");
+		lcd.clear();	
+		lcd.print("Motor Start");
+		lcd.setCursor(0,1);
     }
 
-    if(on){
-      boolean ret = turn_motor(claw_in);
-      if(ret){
-        on = false;
-        claw_in = !claw_in;
-        if(!claw_in){
-          Serial.println("Motor stopped");
+    if(on)
+	{
+		boolean ret = turn_motor(claw_in);
+		if(ret)
+		{
+			on = false;
+			claw_in = !claw_in;
+			if(!claw_in)
+			{
+				Serial.println("Motor stopped");
+				lcd.clear();	
+				lcd.print("Motor Stop");
+				lcd.setCursor(0,1);
+				lcd.print(analogRead(hopper_sensor));
+				delay(2000);
+				on = true;
+				Serial.println("Motor resetting");
+				lcd.clear();	
+				lcd.print("Motor Reset Now");
+			}
+			else
+			{
+				Serial.println("Motor reset");
+				lcd.clear();
+				lcd.print("Motor Reset");
+				boolean ret = ball_collected();
+				if(ret)
+				{
+					blink_led();
 					lcd.clear();	
-					lcd.print("Motor Stop");
-          delay(2000);
-          on = true;
-          Serial.println("Motor resetting");
-					lcd.clear();	
-					lcd.print("Motor Reset Now");
-        }else{
-          Serial.println("Motor reset");
-					lcd.clear();	
-					lcd.print("Motor Reset");
-          boolean ret = ball_collected();
-          if(ret){
-            blink_led();
-						lcd.clear();	
-						lcd.println("Mission");
-						lcd.print("Accomplished");
-          }
-        }  
-      }
+					lcd.print("Mission");
+					lcd.setCursor(0,1);
+					lcd.print("Accomplished");
+				}
+			}  
+		}
     }    
-
     delay(500);
+	Serial.print("claw_in: ");
+	Serial.print(claw_in);
+	Serial.print(" on: ");
+	Serial.print(on);
+	Serial.print(" start: ");
+	Serial.print(start);
+	Serial.print(" engaged: ");
+	Serial.println(engaged);
 }
 
 
@@ -87,56 +106,66 @@ int read_val(int pin){
 }
 
 boolean ball_collected(){
-  return true;
+	return true;
 }  
 
 void blink_led(){
-  Serial.println("Blinking LED");
-  digitalWrite(LED, HIGH);
-  delay(500);
-  digitalWrite(LED, LOW);
+	Serial.println("Blinking LED");
+	digitalWrite(LED, HIGH);
+	delay(500);
+	digitalWrite(LED, LOW);
 }
 
 //-----Drive
 //--------------------
-boolean turn_motor(boolean left){
-  //Runs the motor
-    if(read_val(encoder_sensor) < 600 && on){
-      if(engaged){    //Kill motor
-        Serial.println("Motor Off");        
-        engaged = false;
-        motor_stop();
-        return true;
-      }
+boolean turn_motor(boolean left)
+{
+	//Runs the motor
+    if(read_val(encoder_sensor) < 600 && on)
+	{
+		if(engaged)
+		{    //Kill motor
+			Serial.println("Motor Off");        
+			engaged = false;
+			motor_stop();
+			return true;
+		}
     }    
-    else if (read_val(encoder_sensor) > 600 && on){
-      if(!engaged){
-        engaged = true;
-      }
+    else if (read_val(encoder_sensor) > 600 && on)
+	{
+		if(!engaged)
+		{
+			engaged = true;
+		}
     }
 
     //Output of FSM
-    if(left){
-      motor_l();
+    if(left)
+	{
+		motor_l();
     }
-    else{
-      motor_r();
+    else
+	{
+		motor_r();
     }
     
     return false;
 }
 
-void motor_stop(){
+void motor_stop()
+{
     digitalWrite(motor_left[0], LOW);
     digitalWrite(motor_left[1], LOW);    
 }
 
-void motor_r(){
+void motor_r()
+{
     digitalWrite(motor_left[0], HIGH);
     digitalWrite(motor_left[1], LOW);
 }
 
-void motor_l(){
+void motor_l()
+{
     digitalWrite(motor_left[0], HIGH);
     digitalWrite(motor_left[1], HIGH);    
 }
