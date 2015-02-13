167c3d0f8075d9d7bf09ac3002c4d32e98400de2
Chengzhi Winston Liu <liuche25@localhost.localdomain>
2015-02-13 17:18:00 -0500

    Slight modifications

--- irsensor/src/irsensor.ino
+++ irsensor/src/irsensor.ino
@@ -2,6 +2,8 @@ const int SP_1 = A0;
 const int SP_2 = A1;
 const int SP_3 = A2;
 
+unsigned int lap_time = 0;
+
 void setup()
 {
 	Serial.begin(9600);
@@ -9,10 +11,19 @@ void setup()
 
 void loop()
 {
+	if ((millis() - lap_time) > 500)
+	{
+		display();	
+		lap_time = millis();
+	}	
+}
+
+void display()
+{
 	Serial.print(analogRead(SP_1));
 	Serial.print(" ");
 	Serial.print(analogRead(SP_2));
 	Serial.print(" ");
 	Serial.println(analogRead(SP_3));
-    delay(400);
+
 }
